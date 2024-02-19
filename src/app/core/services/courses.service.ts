f5a136e28716c56ae46eaab0e24894072766ef18
Matias Maguicha <matiasmaguicha13@gmail.com>
2024-02-18 23:21:23 -0300

    Implementacion de APIs para cursos y estudiantes

--- src/app/core/services/courses.service.ts
+++ src/app/core/services/courses.service.ts
@@ -1,86 +1,108 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, mergeMap, of } from 'rxjs';
 import { Course } from '../../dashboard/pages/courses/models';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../../environments/environment.development';
 
-let courses: Course[] = [
-  {
-    id: 1,
-    name: 'Angular Fundamentals',
-    description: 'Curso introductorio de Angular',
-    duration: 20,
-    createdAt: new Date(),
-  },
-  {
-    id: 2,
-    name: 'React Basics',
-    description: 'Introducción a React',
-    duration: 15,
-    createdAt: new Date(),
-  },
-  {
-    id: 3,
-    name: 'Vue.js Essentials',
-    description: 'Curso esencial de Vue.js',
-    duration: 18,
-    createdAt: new Date(),
-  },
-  {
-    id: 4,
-    name: 'Node.js for Beginners',
-    description: 'Introducción a Node.js',
-    duration: 25,
-    createdAt: new Date(),
-  },
-  {
-    id: 5,
-    name: 'Express.js Crash Course',
-    description: 'Curso rápido de Express.js',
-    duration: 12,
-    createdAt: new Date(),
-  },
-]
+// let courses: Course[] = [
+//   {
+//     id: 1,
+//     name: 'Angular Fundamentals',
+//     description: 'Curso introductorio de Angular',
+//     duration: 20,
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 2,
+//     name: 'React Basics',
+//     description: 'Introducción a React',
+//     duration: 15,
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 3,
+//     name: 'Vue.js Essentials',
+//     description: 'Curso esencial de Vue.js',
+//     duration: 18,
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 4,
+//     name: 'Node.js for Beginners',
+//     description: 'Introducción a Node.js',
+//     duration: 25,
+//     createdAt: new Date(),
+//   },
+//   {
+//     id: 5,
+//     name: 'Express.js Crash Course',
+//     description: 'Curso rápido de Express.js',
+//     duration: 12,
+//     createdAt: new Date(),
+//   },
+// ]
 
 @Injectable()
 
 export class CoursesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   getCourses() {    
-    return of(courses);
+    //return of(courses);
+
+    return this.httpClient
+      .get<Course[]>(`${environment.apiURL}/courses`, {  })
+      .pipe(
+        catchError((error) => {
+          alert('Error al cargar los cursos');
+          return of([]);
+        })
+      );
   }
 
-  addCourse(newcourse: Course) {
-    courses = [
-      ...courses,
-      {
-        ...newcourse,
-        id: this.getNextUniqueId(), //courses.length + 1,
-      },
-    ];
-    return this.getCourses();
+  addCourse(newCourse: Course) {
+    // courses = [
+    //   ...courses,
+    //   {
+    //     ...newcourse,
+    //     id: this.getNextUniqueId(), //courses.length + 1,
+    //   },
+    // ];
+    // return this.getCourses();
+
+    return this.httpClient
+    .post<Course>(`${environment.apiURL}/courses`, { ...newCourse, })
+    .pipe(mergeMap(() => this.getCourses()));
   }
 
-  private getNextUniqueId(): number {
-    // Encontramos el máximo ID actual
-    const maxId = courses.length > 0 ? Math.max(...courses.map(e => e.id)) : 0;
+  // private getNextUniqueId(): number {
+  //   // Encontramos el máximo ID actual
+  //   const maxId = courses.length > 0 ? Math.max(...courses.map(e => e.id)) : 0;
     
-    // Incrementamos el máximo ID para obtener un nuevo ID único
-    return maxId + 1;
-  }
+  //   // Incrementamos el máximo ID para obtener un nuevo ID único
+  //   return maxId + 1;
+  // }
 
-  updateCourseById(courseId: number, updatecourse: Course) {
-    courses = courses.map((course) => course.id === courseId ? { ...course, ...updatecourse } : course);
-    return this.getCourses();
+  updateCourseById(courseId: number, updateCourse: Course) {
+    // courses = courses.map((course) => course.id === courseId ? { ...course, ...updatecourse } : course);
+    // return this.getCourses();
+
+    return this.httpClient.put<Course>(`${environment.apiURL}/courses/${courseId}`, { ...updateCourse,  })
+    .pipe(mergeMap(() => this.getCourses()));
   }
 
   deleteCourseById(courseId: number) {
-    courses = courses.filter((el) => el.id != courseId); // filtro y piso la variable
-    return this.getCourses(); //llamo de nuevo a la funcion para q muestre la data actualizada
+    // courses = courses.filter((el) => el.id != courseId); // filtro y piso la variable
+    // return this.getCourses(); //llamo de nuevo a la funcion para q muestre la data actualizada
+
+    return this.httpClient.delete<Course>(`${environment.apiURL}/courses/${courseId}`)
+      .pipe(mergeMap(() => this.getCourses()));
   }
   
   getCourseById(id: number | string): Observable<Course | undefined> {
-    return of(courses.find((course) => course.id == id));
+    //return of(courses.find((course) => course.id == id));
+    return this.httpClient.get<Course>(`${environment.apiURL}/courses/${id}`);
   }
 
 }
--- src/app/core/services/students.service.ts
+++ src/app/core/services/students.service.ts
@@ -1,91 +1,113 @@
 import { Injectable } from '@angular/core';
 import { Student } from '../../dashboard/pages/students/models/student';
-import { Observable, of } from 'rxjs';
+import { Observable, catchError, mergeMap, of } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { HttpClient } from '@angular/common/http';
 
-let students: Student[] = [
-  {
-    id: 1,
-    firstName: 'Juan',
-    lastName: 'Gomez',
-    email: 'juan.gomez@mail.com',
-    document: 36523122,
-    birthDate: new Date(1991, 2, 12)
-  },
-  {
-    id: 2,
-    firstName: 'Maria',
-    lastName: 'Rodriguez',
-    email: 'maria.rodriguez@mail.com',
-    document: 36535122,
-    birthDate: new Date(1993, 5, 12)
-  },
-  {
-    id: 3,
-    firstName: 'Lucas',
-    lastName: 'Lopez',
-    email: 'lucas.lopez@mail.com',
-    document: 32323122,
-    birthDate: new Date(1995, 2, 2)
-  },
-  {
-    id: 4,
-    firstName: 'Ana',
-    lastName: 'Fernandez',
-    email: 'ana.fernandez@mail.com',
-    document: 36523422,
-    birthDate: new Date(1990, 5, 11)
-  },
-  {
-    id: 5,
-    firstName: 'Diego',
-    lastName: 'Diaz',
-    email: 'diego.diaz@mail.com',
-    document: 36523100,
-    birthDate: new Date(1990, 10, 10)
-  }
-];
+// let students: Student[] = [
+//   {
+//     id: 1,
+//     firstName: 'Juan',
+//     lastName: 'Gomez',
+//     email: 'juan.gomez@mail.com',
+//     document: 36523122,
+//     birthDate: new Date(1991, 2, 12)
+//   },
+//   {
+//     id: 2,
+//     firstName: 'Maria',
+//     lastName: 'Rodriguez',
+//     email: 'maria.rodriguez@mail.com',
+//     document: 36535122,
+//     birthDate: new Date(1993, 5, 12)
+//   },
+//   {
+//     id: 3,
+//     firstName: 'Lucas',
+//     lastName: 'Lopez',
+//     email: 'lucas.lopez@mail.com',
+//     document: 32323122,
+//     birthDate: new Date(1995, 2, 2)
+//   },
+//   {
+//     id: 4,
+//     firstName: 'Ana',
+//     lastName: 'Fernandez',
+//     email: 'ana.fernandez@mail.com',
+//     document: 36523422,
+//     birthDate: new Date(1990, 5, 11)
+//   },
+//   {
+//     id: 5,
+//     firstName: 'Diego',
+//     lastName: 'Diaz',
+//     email: 'diego.diaz@mail.com',
+//     document: 36523100,
+//     birthDate: new Date(1990, 10, 10)
+//   }
+// ];
 
 @Injectable()
 
 export class StudentsService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
 
   getStudents() {    
-    return of(students);
+    //return of(students);
+
+    return this.httpClient
+      .get<Student[]>(`${environment.apiURL}/students`, {  })
+      .pipe(
+        catchError((error) => {
+          alert('Error al cargar los estudiantes');
+          return of([]);
+        })
+      );
   }
 
   addStudent(newStudent: Student) {
-    students = [
-      ...students,
-      {
-        ...newStudent,
-        id: this.getNextUniqueId(), //students.length + 1,
-      },
-    ];
-    return this.getStudents();
+    // students = [
+    //   ...students,
+    //   {
+    //     ...newStudent,
+    //     id: this.getNextUniqueId(), //students.length + 1,
+    //   },
+    // ];
+    // return this.getStudents();
+
+    return this.httpClient
+    .post<Student>(`${environment.apiURL}/students`, { ...newStudent, })
+    .pipe(mergeMap(() => this.getStudents()));
   }
 
-  private getNextUniqueId(): number {
-    // Encontramos el máximo ID actual
-    const maxId = students.length > 0 ? Math.max(...students.map(e => e.id)) : 0;
+  // private getNextUniqueId(): number {
+  //   // Encontramos el máximo ID actual
+  //   const maxId = students.length > 0 ? Math.max(...students.map(e => e.id)) : 0;
     
-    // Incrementamos el máximo ID para obtener un nuevo ID único
-    return maxId + 1;
-  }
+  //   // Incrementamos el máximo ID para obtener un nuevo ID único
+  //   return maxId + 1;
+  // }
 
   updateStudentById(studentId: number, updateStudent: Student) {
-    students = students.map((student) => student.id === studentId ? { ...student, ...updateStudent } : student);
-    return this.getStudents();
+    // students = students.map((student) => student.id === studentId ? { ...student, ...updateStudent } : student);
+    // return this.getStudents();
+
+    return this.httpClient.put<Student>(`${environment.apiURL}/students/${studentId}`, { ...updateStudent,  })
+      .pipe(mergeMap(() => this.getStudents()));
   }
 
   deleteStudentById(StudentId: number) {
-    students = students.filter((el) => el.id != StudentId); // filtro y piso la variable
-    return this.getStudents(); //llamo de nuevo a la funcion para q muestre la data actualizada
+    // students = students.filter((el) => el.id != StudentId); // filtro y piso la variable
+    // return this.getStudents(); //llamo de nuevo a la funcion para q muestre la data actualizada
+
+    return this.httpClient.delete<Student>(`${environment.apiURL}/students/${StudentId}`)
+      .pipe(mergeMap(() => this.getStudents()));
   }
   
   getStudentById(id: number | string): Observable<Student | undefined> {
-    return of(students.find((student) => student.id == id));
+    //return of(students.find((student) => student.id == id));
+    return this.httpClient.get<Student>(`${environment.apiURL}/students/${id}`);
   }
 
 }
